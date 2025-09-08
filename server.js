58cf5277a92377ca39c29a4ceab76fd84a5a2a43
luis.brito <luis.brito@itpraxis.cl>
2025-09-08 16:12:19 -0300

    Prueba 48 nueva API para el catalogot

--- server.js
+++ server.js
@@ -102,9 +102,7 @@ const getAvailableDates = async (bbox, maxCloudCoverage) => {
             "datetime": "2020-01-01T00:00:00Z/2025-01-01T23:59:59Z",
             "collections": ["sentinel-2-l2a"],
             "limit": 100,
-            {
-              "filtro" : "eo:cloud_cover > 90" 
-            }
+            "filtro" : "eo:cloud_cover > 90" 
         };
 
         console.log('Enviando payload al catálogo:', JSON.stringify(payload));
