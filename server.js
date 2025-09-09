895ad6bfe6b1954e59f8bb898a70f76b9d1a58f0
luis.brito <luis.brito@itpraxis.cl>
2025-09-08 21:09:17 -0300

    Prueba 64 cambio NDVI resolucion 1500

--- server.js
+++ server.js
@@ -256,18 +256,18 @@ const getNdviAverage = async ({ geometry, date }) => {
                 function setup() {
                     return {
                         input: [{ bands: ["B08", "B04", "dataMask"], units: "REFLECTANCE" }],
-                        // El output del evalscript no necesita bands ni sampleType para STATS
                         output: { bands: 1 } 
                     };
                 }
                 function evaluatePixel(samples) {
                     if (samples.dataMask === 0) {
-                        return [NaN]; // Retorna NaN para valores no válidos
+                        return [NaN]; 
                     }
                     const nir = samples.B08;
                     const red = samples.B04;
-                    // El valor promedio se calculará sobre este valor
-                    return [(nir - red) / (nir + red)]; 
+                    const ndvi = (nir - red) / (nir + red);
+                    // ❌ CAMBIO CRÍTICO: DEBES DEVOLVER UN VALOR ÚNICO, NO UN ARREGLO
+                    return ndvi; 
                 }
             `,
             process: {
