0362ace0e04abac81d9296fbf5eab9a6559a84f3
luis.brito <luis.brito@itpraxis.cl>
2025-09-22 20:20:50 -0300

    Agrega imagenes radar 40

--- server.js
+++ server.js
@@ -423,7 +423,7 @@ const fetchSentinel1Radar = async ({ geometry, date }) => {
                         coordinates: geometry
                     }
                 },
-                 [
+                 data: [
                     {
                         dataFilter: {
                             timeRange: {
